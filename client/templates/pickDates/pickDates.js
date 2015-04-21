59f781b05cc842681efedcbb1357bc8c3fee2bfc
maria-faulisi <mjfaulisi@gmail.com>
2015-04-21 17:10:48 -0500

    restrict months viewed

--- client/templates/pickDates/pickDates.js
+++ client/templates/pickDates/pickDates.js
@@ -1,33 +1,75 @@
+var targetMonthYear;
+var prevMonth;
+var nextMonth;
+var eventData = [];
+
 
 Template.pickDates.onRendered(function(){
 	//get the target month and year from db
-	var targetMonthYear = moment(this.data.eventMonthYear);
-	//get previous Month
-	var prevMonth = moment(this.data.eventMonthYear).subtract(1, 'M');
-	//get next Month
-	var nextMonth = moment(this.data.eventMonthYear).add(1, 'M');
-	console.log(prevMonth);
-	console.log(targetMonthYear);
-	console.log(nextMonth);
-
-    /*Getting Event data from iron-router and storing it into an array*/
-    var eventData = this.data.dates;
-
-
-    somePeople = this.data.groupMembers;
-
-    Session.set('somePeople', this.data.groupMembers);
+	targetMonthYear = this.data.eventMonthYear;
+	Session.set('calendarTarget', targetMonthYear);
 
     var calendar = $('#pickDatesCal').fullCalendar({
+    	header:{
+    		today: false,
+    		left: 'prev',
+    		right: 'next',
+    		center: 'title'
+    	},
     	defaultDate: targetMonthYear,
-    	start: prevMonth,
-    	end: nextMonth,
-        /*dayRender function handling the intial rendering of days on page load*/
-        dayRender: function (date, cell) {
-            /*For each item in the eventData onLoad adding the toggleOn class*/
-            $.each(eventData,function(index,value){
-                $("td[data-date='"+value+"']").addClass('toggleOn');
-            });
-        }
-    })
+    	eventDurationEditable: false,
+		dayClick:function(date,jsEvent,view){
+	        /*OnClick storing day in DayClicked in a format of YYYY-MM-DD*/
+	        var DayClicked =  $.fullCalendar.moment(date).format('YYYY-MM-DD');
+	        $(this).toggleClass( "toggleOn" );
+	        /*If the day has a class of toggleOn pusinh into the eventData array else looping array and removing it*/
+	        if($(this).hasClass("toggleOn"))
+	        {
+	            console.log('On');
+	            eventData.push(DayClicked);
+	        }else{
+	            console.log('Off');
+	            for(var z = eventData.length; z--;) {
+	                if (eventData[z] === DayClicked) {
+	                    eventData.splice(z, 1);
+	                }
+	            }
+	        }
+	        console.log(eventData);
+        }//closes dayClick
+
+    })//closes calendar
+
+});//closes onRendered
+
+Template.pickDates.events({
+
+	'click .fc-prev-button': function(){
+		var $this = $(this);
+
+    	/*var thisEvents = $._data($this[0], "events");*/
+    	var chosenDate = Session.get('calendarTarget');//date object
+    	var chosenMoment = moment(chosenDate);//date converted to moment
+
+    	var prevChosenMoment = moment(chosenDate).subtract(1, 'M');
+    	var prevChosenDate = prevChosenMoment.toDate();
+    	console.log(prevChosenDate);
+    	Session.set('prevChosenDate', prevChosenDate);
+    	var calCurrent = $('#pickDatesCal').fullCalendar('getDate');
+
+    	if(calCurrent.isSame(prevChosenMoment)){
+    		/*console.log(thisEvents);
+    		$this.unbind('click');
+    		console.log(thisEvents);*/
+    		$('#pickDatesCal').fullCalendar('gotoDate', Session.get('prevChosenDate'));
+    		$('#pickDatesCal').fullCalendar('render');
+
+    	}
+	}
+
 });
+
+Template.pickDates.helpers({
+
+});
+
